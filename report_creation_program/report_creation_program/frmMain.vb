be035dbab5487e6a4b0030e34abf3a531ac46499
Ii Blue <bluehamster648@gmail.com>
2021-07-31 13:19:36 -0400

    Added try...catches to the update clocks

--- report_creation_program/report_creation_program/frmMain.vb
+++ report_creation_program/report_creation_program/frmMain.vb
@@ -108,112 +108,127 @@ Public Class frmMain
 
     Private Sub tmrUpdateLocalConfig_Tick(sender As Object, e As EventArgs) Handles tmrUpdateLocalConfig.Tick
 
-        ' Elapse time
-        intElapsedTimeUpdateConfig += 1
+        Try
 
-        ' Has 5 minutes elapsed?
-        If (intElapsedTimeUpdateConfig >= 30) Then ' 60 * 5 * 2
-            ' Get db update
-            If OpenDatabaseConnectionSQLServer() = False Then
+            ' Elapse time
+            intElapsedTimeUpdateConfig += 1
 
-                ' The database is not open
-                MessageBox.Show(Me, "Database connection error." & vbNewLine &
+            ' Has 5 minutes elapsed?
+            If (intElapsedTimeUpdateConfig >= 30) Then ' 60 * 5 * 2
+                ' Get db update
+                If OpenDatabaseConnectionSQLServer() = False Then
+
+                    ' The database is not open
+                    MessageBox.Show(Me, "Database connection error." & vbNewLine &
                             "The form will now close.",
                             Me.Text + " Error",
                             MessageBoxButtons.OK, MessageBoxIcon.Error)
 
-                ' Close the form/application
-                ' Me.Close()
+                    ' Close the form/application
+                    ' Me.Close()
 
-                Exit Sub
+                    Exit Sub
 
+                End If
+
+                ' Init select statement string
+                Dim strSelect As String = ""
+                ' Init select statement Db command
+                Dim cmdSelect As OleDb.OleDbCommand
+                ' Init data reader
+                Dim drSourceTable As OleDb.OleDbDataReader
+                ' Init data table
+                Dim dt As DataTable = New DataTable
+
+                ' Build the select statement
+                strSelect = "SELECT * FROM TReports"
+
+                ' Retrieve all the records 
+                cmdSelect = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
+                drSourceTable = cmdSelect.ExecuteReader
+                ' Get data
+                dt.Load(drSourceTable)
+
+                ' Load from data table
+                strEmailSalesReport = dt(0)("strTargetEmail").ToString
+                strEmailSalesTaxReport = dt(2)("strTargetEmail").ToString
+                strEmailInventoryReport = dt(1)("strTargetEmail").ToString
+                strEmailDespositReport = dt(3)("strTargetEmail").ToString
+
+                blnSalesReportDaily = dt(0)("blnDaily").ToString
+                blnSalesReportMonthly = dt(0)("blnMonthly").ToString
+                blnSalesReportWeekly = dt(0)("blnWeekly").ToString
+                blnSalesReportYearly = dt(0)("blnYearly").ToString
+
+                blnInventoryReportDaily = dt(1)("blnDaily").ToString
+                blnInventoryReportMonthly = dt(1)("blnMonthly").ToString
+                blnInventoryReportWeekly = dt(1)("blnWeekly").ToString
+                blnInventoryReportYearly = dt(1)("blnYearly").ToString
+
+                blnSalesTaxReportDaily = dt(2)("blnDaily").ToString
+                blnSalesTaxReportMonthly = dt(2)("blnMonthly").ToString
+                blnSalesTaxReportWeekly = dt(2)("blnWeekly").ToString
+                blnSalesTaxReportYearly = dt(2)("blnYearly").ToString
+
+                blnDepositReportDaily = dt(3)("blnDaily").ToString
+                blnDepositReportMonthly = dt(3)("blnMonthly").ToString
+                blnDepositReportWeekly = dt(3)("blnWeekly").ToString
+                blnDepositReportYearly = dt(3)("blnYearly").ToString
+
+                dtmDailySalesReport = Convert.ToDateTime(dt(0)("dtDailyReportDate").ToString())
+                dtmDailySalesTaxReport = Convert.ToDateTime(dt(2)("dtDailyReportDate").ToString())
+                dtmDailyInventoryReport = Convert.ToDateTime(dt(1)("dtDailyReportDate").ToString())
+                dtmDailyDepositReport = Convert.ToDateTime(dt(3)("dtDailyReportDate").ToString())
+
+                dtmWeeklySalesReport = Convert.ToDateTime(dt(0)("dtWeeklyReportDate").ToString())
+                dtmWeeklySalesTaxReport = Convert.ToDateTime(dt(2)("dtWeeklyReportDate").ToString())
+                dtmWeeklyInventoryReport = Convert.ToDateTime(dt(1)("dtWeeklyReportDate").ToString())
+                dtmWeeklyDepositReport = Convert.ToDateTime(dt(3)("dtWeeklyReportDate").ToString())
+
+                dtmMonthlySalesReport = Convert.ToDateTime(dt(0)("dtMonthlyReportDate").ToString())
+                dtmMonthlySalesTaxReport = Convert.ToDateTime(dt(2)("dtMonthlyReportDate").ToString())
+                dtmMonthlyInventoryReport = Convert.ToDateTime(dt(1)("dtMonthlyReportDate").ToString())
+                dtmMonthlyDepositReport = Convert.ToDateTime(dt(3)("dtMonthlyReportDate").ToString())
+
+                dtmYearlySalesReport = Convert.ToDateTime(dt(0)("dtYearlyReportDate").ToString())
+                dtmYearlySalesTaxReport = Convert.ToDateTime(dt(2)("dtYearlyReportDate").ToString())
+                dtmYearlyInventoryReport = Convert.ToDateTime(dt(1)("dtYearlyReportDate").ToString())
+                dtmYearlyDepositReport = Convert.ToDateTime(dt(3)("dtYearlyReportDate").ToString())
+
+                ' Reset
+                intElapsedTimeUpdateConfig = 0
             End If
+        Catch excError As Exception
 
-            ' Init select statement string
-            Dim strSelect As String = ""
-            ' Init select statement Db command
-            Dim cmdSelect As OleDb.OleDbCommand
-            ' Init data reader
-            Dim drSourceTable As OleDb.OleDbDataReader
-            ' Init data table
-            Dim dt As DataTable = New DataTable
+            Console.WriteLine("Something went wrong in updating the configuration")
 
-            ' Build the select statement
-            strSelect = "SELECT * FROM TReports"
-
-            ' Retrieve all the records 
-            cmdSelect = New OleDb.OleDbCommand(strSelect, m_conAdministrator)
-            drSourceTable = cmdSelect.ExecuteReader
-            ' Get data
-            dt.Load(drSourceTable)
-
-            ' Load from data table
-            strEmailSalesReport = dt(0)("strTargetEmail").ToString
-            strEmailSalesTaxReport = dt(2)("strTargetEmail").ToString
-            strEmailInventoryReport = dt(1)("strTargetEmail").ToString
-            strEmailDespositReport = dt(3)("strTargetEmail").ToString
-
-            blnSalesReportDaily = dt(0)("blnDaily").ToString
-            blnSalesReportMonthly = dt(0)("blnMonthly").ToString
-            blnSalesReportWeekly = dt(0)("blnWeekly").ToString
-            blnSalesReportYearly = dt(0)("blnYearly").ToString
-
-            blnInventoryReportDaily = dt(1)("blnDaily").ToString
-            blnInventoryReportMonthly = dt(1)("blnMonthly").ToString
-            blnInventoryReportWeekly = dt(1)("blnWeekly").ToString
-            blnInventoryReportYearly = dt(1)("blnYearly").ToString
-
-            blnSalesTaxReportDaily = dt(2)("blnDaily").ToString
-            blnSalesTaxReportMonthly = dt(2)("blnMonthly").ToString
-            blnSalesTaxReportWeekly = dt(2)("blnWeekly").ToString
-            blnSalesTaxReportYearly = dt(2)("blnYearly").ToString
-
-            blnDepositReportDaily = dt(3)("blnDaily").ToString
-            blnDepositReportMonthly = dt(3)("blnMonthly").ToString
-            blnDepositReportWeekly = dt(3)("blnWeekly").ToString
-            blnDepositReportYearly = dt(3)("blnYearly").ToString
-
-            dtmDailySalesReport = Convert.ToDateTime(dt(0)("dtDailyReportDate").ToString())
-            dtmDailySalesTaxReport = Convert.ToDateTime(dt(2)("dtDailyReportDate").ToString())
-            dtmDailyInventoryReport = Convert.ToDateTime(dt(1)("dtDailyReportDate").ToString())
-            dtmDailyDepositReport = Convert.ToDateTime(dt(3)("dtDailyReportDate").ToString())
-
-            dtmWeeklySalesReport = Convert.ToDateTime(dt(0)("dtWeeklyReportDate").ToString())
-            dtmWeeklySalesTaxReport = Convert.ToDateTime(dt(2)("dtWeeklyReportDate").ToString())
-            dtmWeeklyInventoryReport = Convert.ToDateTime(dt(1)("dtWeeklyReportDate").ToString())
-            dtmWeeklyDepositReport = Convert.ToDateTime(dt(3)("dtWeeklyReportDate").ToString())
-
-            dtmMonthlySalesReport = Convert.ToDateTime(dt(0)("dtMonthlyReportDate").ToString())
-            dtmMonthlySalesTaxReport = Convert.ToDateTime(dt(2)("dtMonthlyReportDate").ToString())
-            dtmMonthlyInventoryReport = Convert.ToDateTime(dt(1)("dtMonthlyReportDate").ToString())
-            dtmMonthlyDepositReport = Convert.ToDateTime(dt(3)("dtMonthlyReportDate").ToString())
-
-            dtmYearlySalesReport = Convert.ToDateTime(dt(0)("dtYearlyReportDate").ToString())
-            dtmYearlySalesTaxReport = Convert.ToDateTime(dt(2)("dtYearlyReportDate").ToString())
-            dtmYearlyInventoryReport = Convert.ToDateTime(dt(1)("dtYearlyReportDate").ToString())
-            dtmYearlyDepositReport = Convert.ToDateTime(dt(3)("dtYearlyReportDate").ToString())
-
-            ' Reset
-            intElapsedTimeUpdateConfig = 0
-        End If
+        End Try
 
     End Sub
 
     Private Sub tmrCheckIfReportRun_Tick(sender As Object, e As EventArgs) Handles tmrCheckIfReportRun.Tick
 
-        ' Elapse time
-        intElapsedTimeRunReport += 1
+        Try
 
-        ' Has 5 minutes elapsed?
-        If (intElapsedTimeRunReport >= 1) Then
+            ' Elapse time
+            intElapsedTimeRunReport += 1
 
-            ' Run sales report
-            AutomateSalesReport()
+            ' Has 5 minutes elapsed?
+            If (intElapsedTimeRunReport >= 1) Then
 
-            ' Reset
-            intElapsedTimeRunReport = 0
+                ' Run sales report
+                AutomateSalesReport()
 
-        End If
+                ' Reset
+                intElapsedTimeRunReport = 0
+
+            End If
+
+        Catch excError As Exception
+
+            Console.WriteLine("Something went wrong in generating the reports")
+
+        End Try
 
     End Sub
 
