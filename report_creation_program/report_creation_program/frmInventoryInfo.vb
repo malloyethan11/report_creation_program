ce07cd5170277c40e58dca8ee30c52f1965b5d52
Ii Blue <bluehamster648@gmail.com>
2021-08-04 20:04:40 -0400

    Handle errors better

--- report_creation_program/report_creation_program/frmInventoryInfo.vb
+++ report_creation_program/report_creation_program/frmInventoryInfo.vb
@@ -27,36 +27,49 @@
 
     Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
 
-        Dim strToEmail As String
+        Try
+            Dim strToEmail As String
 
-        ' validate input
-        If txtEmail.Text = "" Or System.Text.RegularExpressions.Regex.IsMatch(txtEmail.Text, "^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$") = False Then
-            txtEmail.BackColor = Color.Yellow
-            MessageBox.Show("Please enter a valid email address.")
+            ' validate input
+            If txtEmail.Text = "" Or System.Text.RegularExpressions.Regex.IsMatch(txtEmail.Text, "^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$") = False Then
+                txtEmail.BackColor = Color.Yellow
+                MessageBox.Show("Please enter a valid email address.")
 
-        Else
+            Else
 
-            strToEmail = txtEmail.Text
+                strToEmail = txtEmail.Text
 
-            txtEmail.BackColor = Color.White
+                txtEmail.BackColor = Color.White
 
-            ' run inventory report
-            Dim blnResult As Boolean = RunInventoryReport(Me, False)
+                ' run inventory report
+                Dim blnResult As Boolean = RunInventoryReport(Me, False)
 
+                'GC.Collect()
+                'GC.WaitForPendingFinalizers()
+
+                '' email inventory report
+                'strFile = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase) + "\InventoryReport.xlsx"
+
+                'strFile = strFile.Remove(0, 6)
+
+                Threading.Thread.Sleep(3000)
+
+                If blnResult = True Then
+                    SendMail(txtEmail.Text, "TeamBeesCapstone@gmail.com", "Inventory Report", "", "TeamBeesCapstone@gmail.com", "cincystate123", "InventoryReport.xlsx", False)
+                End If
+            End If
+
+        Catch excError As Exception
+
+            MessageBox.Show("Something went wrong in creating that report." & vbNewLine & "Error: " & excError.Message, "Error!")
+
+        Finally
+
+            ' In case garbage wasn't collected
             GC.Collect()
             GC.WaitForPendingFinalizers()
 
-            '' email inventory report
-            'strFile = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase) + "\InventoryReport.xlsx"
-
-            'strFile = strFile.Remove(0, 6)
-
-            Threading.Thread.Sleep(3000)
-
-            If blnResult = True Then
-                SendMail(txtEmail.Text, "TeamBeesCapstone@gmail.com", "Inventory Report", "", "TeamBeesCapstone@gmail.com", "cincystate123", "InventoryReport.xlsx", False)
-            End If
-        End If
+        End Try
     End Sub
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
